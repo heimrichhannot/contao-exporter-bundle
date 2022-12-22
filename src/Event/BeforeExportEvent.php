115962fc895426085139ca65add7ca64aa9dcff6
Paul Koreng <p.koreng@heimrich-hannot.de>
2022-12-22 14:48:09 +0100

    fix php8 deprecation warning

--- src/Event/BeforeExportEvent.php
+++ src/Event/BeforeExportEvent.php
@@ -1,16 +1,12 @@
 <?php
-/**
- * Contao Open Source CMS
+
+/*
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
  *
- * Copyright (c) 2018 Heimrich & Hannot GmbH
- *
- * @author  Thomas Körner <t.koerner@heimrich-hannot.de>
- * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ * @license LGPL-3.0-or-later
  */
 
-
 namespace HeimrichHannot\ContaoExporterBundle\Event;
-
 
 use Contao\Model;
 use HeimrichHannot\ContaoExporterBundle\Exporter\AbstractExporter;
@@ -31,10 +27,6 @@ class BeforeExportEvent extends Event
     /**
      * @var string
      */
-    private $fileDir;
-    /**
-     * @var string
-     */
     private $fileName;
     /**
      * @var ExporterModel
@@ -44,27 +36,25 @@ class BeforeExportEvent extends Event
      * @var AbstractExporter
      */
     private $exporter;
+    /**
+     * @var string
+     */
+    private $fileDir;
 
-    public function __construct($entity, array $fields, string $fileDir = '', string $fileName, AbstractExporter $exporter)
+    public function __construct($entity, array $fields, string $fileName, AbstractExporter $exporter, string $fileDir = '')
     {
         $this->entity = $entity;
         $this->fields = $fields;
-        $this->fileDir = $fileDir;
         $this->fileName = $fileName;
         $this->exporter = $exporter;
+        $this->fileDir = $fileDir;
     }
 
-    /**
-     * @return AbstractExporter
-     */
     public function getExporter(): AbstractExporter
     {
         return $this->exporter;
     }
 
-    /**
-     * @param AbstractExporter $exporter
-     */
     public function setExporter(AbstractExporter $exporter): void
     {
         $this->exporter = $exporter;
@@ -86,51 +76,33 @@ class BeforeExportEvent extends Event
         $this->entity = $entity;
     }
 
-    /**
-     * @return array
-     */
     public function getFields(): array
     {
         return $this->fields;
     }
 
-    /**
-     * @param array $fields
-     */
     public function setFields(array $fields): void
     {
         $this->fields = $fields;
     }
 
-    /**
-     * @return string
-     */
-    public function getFileDir(): string
-    {
-        return $this->fileDir;
-    }
-
-    /**
-     * @param string $fileDir
-     */
-    public function setFileDir(string $fileDir): void
-    {
-        $this->fileDir = $fileDir;
-    }
-
-    /**
-     * @return string
-     */
     public function getFileName(): string
     {
         return $this->fileName;
     }
 
-    /**
-     * @param string $fileName
-     */
     public function setFileName(string $fileName): void
     {
         $this->fileName = $fileName;
+    }
+
+    public function getFileDir(): string
+    {
+        return $this->fileDir;
+    }
+
+    public function setFileDir(string $fileDir): void
+    {
+        $this->fileDir = $fileDir;
     }
 }
